0d8ee215c10a71536294319a2668fa3ff06f9b94
DivyaSreeKala <divyasreekala99@gmail.com>
2024-09-20 13:05:19 +0530

    day 20

--- day19/app.js
+++ day19/app.js
@@ -8,10 +8,13 @@ app.use(morgan('dev'))
 // const basicRoutes = require('./routes/basicroutes')
 // app.use('/user',basicRoutes);
 
-app.set('view engine','ejs');
-app.set('views',__dirname+'/views');//the 'views' in first param is the same does not depend on folder name
+app.set('view engine','ejs');// set ejs as the template engine
+app.set('views',__dirname+'/views');//the 'views' in first param (it is a keyword)is the same does not depend on folder name
+// and '/views' is the path
 
-app.use(express.static('public'))
+app.use(express.static('public'))//states that all the static files are in the public folder(so whenever the sttic files (mentioned below)are accessed the js engine looks in the public folder
+//images,pdf,documents,css,js file etc.. are examples of static files
+
 
 const nav = [
     {
@@ -22,9 +25,14 @@ const nav = [
     }
 ]
 
-const basicRoutes = require('./routes/basicroutes')(nav);//to pass nav to basicroutes.js
-app.use('/user',basicRoutes);
+const basicRoutes = require('./routes/basicroutes')(nav);//the module exported from the basicroutes file is required here
+// the function is reqired from the basicroutes file so nav is passed to it
+//to pass nav to the function in basicroutes.js 
+app.use('/user',basicRoutes);//'/user' is the path to get basicroutes????
+
+
 
 app.listen('5000',()=>{
     console.log("Server started at port 5000")
+    console.log(__dirname)
 })
--- day19/routes/basicroutes.js
+++ day19/routes/basicroutes.js
@@ -11,18 +11,20 @@ const data = [{
     eid:"e102"
 }]
 
-function allRoutes(nav){
+function allRoutes(nav){//this fn is used in case of ejs if there is something to pass which is common to all api 
+    //here fn is defined only because the nav is to be passed fro app.js
+//in normal crud this is not required
 
-router.get('/employee',(req,res)=>{
+router.get('/employee',(req,res)=>{//to render the employee page on the path/user/employee
     // res.send(employee)
-    res.render("employee",{//employee is the ejs file name
-        title:'Employeelist',
+    res.render("employee",{//employee is the ejs file name to be rendered
+        title:'Employeelist',//title of the page
         data, //data in the file
         nav
     })
 })
 
-router.get('/employeeform',(req,res)=>{
+router.get('/employeeform',(req,res)=>{//to render the employeeform page on the path /user/employeeform
     // res.send(employee)
     res.render("employeeform",{//employeeform is the ejs file name
         title:'Employee Form',
@@ -35,4 +37,4 @@ return router;
 
 
 // module.exports = router;
-module.exports = allRoutes;
+module.exports = allRoutes;//fn name is exported from the basicroutes file
